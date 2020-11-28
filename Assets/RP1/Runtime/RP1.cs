639e45ddd9f315120ceb93d9fdc7d819007def07
AliceEngineTech <open@battlefire.cn>
2020-11-28 10:49:21 +0800

    搭建SRP开发环境
    Signed-off-by: AliceEngineTech <open@battlefire.cn>

---
+++ Assets/RP1/Runtime/RP1.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RP1 : RenderPipeline
+{
+    protected override void Render(ScriptableRenderContext context, Camera[] cameras)
+    {
+    }
+}
---
+++ Assets/RP1/Runtime/RP1Asset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "RenderPipeline/RP1")]
+public class RP1Asset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline()
+    {
+        return new RP1();
+    }
+}
