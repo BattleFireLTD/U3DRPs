745b03c3bd63a8faff2b8194eb6c51d8d2b2bb06
AliceEngineTech <open@battlefire.cn>
2020-11-29 16:15:35 +0800

    擦除画布
    Signed-off-by: AliceEngineTech <open@battlefire.cn>

--- Assets/RP1/Runtime/RP1.cs
+++ Assets/RP1/Runtime/RP1.cs
@@ -3,7 +3,28 @@ using UnityEngine.Rendering;
 
 public class RP1 : RenderPipeline
 {
+    CommandBuffer commandbuffer = new CommandBuffer() { name="RP1Command"};
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
+        for(int i = 0; i < cameras.Length; ++i)
+        {
+            Camera camera = cameras[i];
+            CameraClearFlags flags = camera.clearFlags;
+            if (flags == CameraClearFlags.Skybox)
+            {
+                context.DrawSkybox(camera);
+            }
+            else
+            {
+                commandbuffer.ClearRenderTarget(
+                    flags <= CameraClearFlags.Depth,
+                    flags <= CameraClearFlags.SolidColor,
+                    new Color(0.1f, 0.4f, 0.6f)
+                );
+                context.ExecuteCommandBuffer(commandbuffer);
+                commandbuffer.Clear();
+            }
+            context.Submit();
+        }
     }
 }
